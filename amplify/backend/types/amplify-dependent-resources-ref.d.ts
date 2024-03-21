2e4df9a598d5cc45d42411c9111e739b6cb5baf1
chou-dai <c.daiki0916@gmail.com>
2024-03-22 00:00:52 +0900

    setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytemplate": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifytemplate19eaa4c5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/graphql/queries.ts
@@ -0,0 +1,78 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+import * as APITypes from "../API";
+type GeneratedQuery<InputType, OutputType> = string & {
+  __generatedQueryInput: InputType;
+  __generatedQueryOutput: OutputType;
+};
+
+export const getTask = /* GraphQL */ `query GetTask($id: ID!) {
+  getTask(id: $id) {
+    id
+    title
+    description
+    status
+    createdAt
+    updatedAt
+    __typename
+  }
+}
+` as GeneratedQuery<APITypes.GetTaskQueryVariables, APITypes.GetTaskQuery>;
+export const listTasks = /* GraphQL */ `query ListTasks(
+  $filter: ModelTaskFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTasks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      title
+      description
+      status
+      createdAt
+      updatedAt
+      __typename
+    }
+    nextToken
+    __typename
+  }
+}
+` as GeneratedQuery<APITypes.ListTasksQueryVariables, APITypes.ListTasksQuery>;
+export const getPrivateNote = /* GraphQL */ `query GetPrivateNote($id: ID!) {
+  getPrivateNote(id: $id) {
+    id
+    content
+    createdAt
+    updatedAt
+    owner
+    __typename
+  }
+}
+` as GeneratedQuery<
+  APITypes.GetPrivateNoteQueryVariables,
+  APITypes.GetPrivateNoteQuery
+>;
+export const listPrivateNotes = /* GraphQL */ `query ListPrivateNotes(
+  $filter: ModelPrivateNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listPrivateNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      content
+      createdAt
+      updatedAt
+      owner
+      __typename
+    }
+    nextToken
+    __typename
+  }
+}
+` as GeneratedQuery<
+  APITypes.ListPrivateNotesQueryVariables,
+  APITypes.ListPrivateNotesQuery
+>;
